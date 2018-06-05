2b446cf5c2d6ed16ea5dad1ec320d74f4d1d7947
Justin Wiley <jlairdwiley@yahoo.com>
2018-06-05 01:11:52 -0400

    7th commit -- project ready for submission;               all code working properly;               landscape layout added;               all code commented

--- app/src/main/java/com/example/justin/myquiz/MainActivity.java
+++ app/src/main/java/com/example/justin/myquiz/MainActivity.java
@@ -3,182 +3,143 @@ package com.example.justin.myquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-//import android.widget.CheckBox;
 import android.widget.CheckBox;
 import android.widget.EditText;
-//import android.widget.TextView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //  Declare RadioButton views for multiple choice answers
+    RadioButton answerOne;
+    RadioButton answerTwo;
+    RadioButton answerFive;
+    RadioButton answerTen;
 
-    //  String variable to hold player name; used in Toast when score displayed.
-    //  Defaults to "Player" if no name is entered.
-    String playerName = "Player";
-
-    //  EditText views for the fill-in-the-blank answers
+    //  Declare EditText views for the fill-in-the-blank answers
     EditText answerThree;
     EditText answerFour;
     EditText answerSeven;
     EditText answerEight;
     EditText answerNine;
 
+    //  Declare CheckBox views for Question 6
     CheckBox attendedHarvard;
     CheckBox asstSecyNavy;
     CheckBox governorNewYork;
 
-    //  Boolean values for each question:  false for incorrect; true for correct.
-    //  All variables initialized to false.
-    boolean question1 = false;
-    boolean question2 = false;
-    boolean question3 = false;
-    boolean question4 = false;
-    boolean question5 = false;
-    boolean question6 = false;
-    boolean question7 = false;
-    boolean question8 = false;
-    boolean question9 = false;
-    boolean question10 = false;
-
+    //  Boolean values for each question:  false for incorrect; true for correct
+    //  All variables initialized to false
+    boolean questionOneCorrect = false;
+    boolean questionTwoCorrect = false;
+    boolean questionThreeCorrect = false;
+    boolean questionFourCorrect = false;
+    boolean questionFiveCorrect = false;
+    boolean questionSixCorrect = false;
+    boolean questionSevenCorrect = false;
+    boolean questionEightCorrect = false;
+    boolean questionNineCorrect = false;
+    boolean questionTenCorrect = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //  Find RadioButton views for Questions 1, 2, 5, 10
+        answerOne = findViewById(R.id.span_am_war);
+        answerTwo = findViewById(R.id.wm_mckinley);
+        answerFive = findViewById(R.id.niece);
+        answerTen = findViewById(R.id.city_tehran);
+
+        //  Find EditText views for Questions 3, 4, 7, 8, 9
         answerThree = findViewById(R.id.answer3);
         answerFour = findViewById(R.id.answer4);
         answerSeven = findViewById(R.id.answer7);
         answerEight = findViewById(R.id.answer8);
         answerNine = findViewById(R.id.answer9);
 
+        //  Find CheckBox views for Question 6
         attendedHarvard = findViewById(R.id.attended_harvard);
         asstSecyNavy = findViewById(R.id.asst_secy_navy);
         governorNewYork = findViewById(R.id.governor_new_york);
     }
 
     /**
-     * This method reads the name entered in the EditText view, and reads it into playerName.
-     * Called by displayScore
+     * Method to check each RadioButton that specifies a correct answer; sets boolean questionXXXCorrect
+     * accordingly
      */
-    private void getPlayerName() {
+    private void checkRadioButtonAnswers() {
+        //  RadioButton isChecked method found on StackOverflow:
+        //  https://stackoverflow.com/questions/11050074/how-to-check-if-radiobutton-is-checked
 
-//        EditText quizNameTextView = (EditText) findViewById(R.id.player_name_edit_text);
-//        playerName = quizNameTextView.getText().toString();
+        //  check each RadioButton view to see if they're checked
+        questionOneCorrect = answerOne.isChecked();
+        questionTwoCorrect = answerTwo.isChecked();
+        questionFiveCorrect = answerFive.isChecked();
+        questionTenCorrect = answerTen.isChecked();
     }
 
     /**
-     * This method performs an "if-else" statement on each question to see if the correct
-     * radio button was clicked.  The "if" part will set a boolean value to true for a
-     * correct answer, and the "else" part will set the boolean to false if either of the
-     * incorrect answers is checked.  Called whenever a radio button is clicked.
-     *
-     * @param view of RadioButton that is checked
+     * Method to check each EditText view for the correct answer; use equalsIgnoreCase method to see if input text
+     * matches answer string retrieved from res/strings; set boolean of questionXXXCorrect accordingly
      */
-    public void checkCorrect(View view) {
-
-        // RadioButton syntax and methods found at the Android Developer website:
-        // https://developer.android.com/guide/topics/ui/controls/radiobutton
-
-        // check question 1
-        if (view.getId() == R.id.span_am_war)
-            question1 = true;
-        else if ((view.getId() == R.id.w_w_i) || (view.getId() == R.id.mex_am_war))
-            question1 = false;
-
-        // check question 2
-        if (view.getId() == R.id.wm_mckinley)
-            question2 = true;
-        else if ((view.getId() == R.id.james_garfield) || (view.getId() == R.id.abe_lincoln))
-            question2 = false;
-
-        // check question 5
-        if (view.getId() == R.id.asst_secty_navy)
-            question5 = true;
-        else if ((view.getId() == R.id.insp_gen_army) || (view.getId() == R.id.pres_army_war_college))
-            question5 = false;
-
-        // check question 7
-        if (view.getId() == R.id.niece)
-            question7 = true;
-        else if ((view.getId() == R.id.second_cousin) || (view.getId() == R.id.stepdaughter))
-            question7 = false;
-
-        // check question 8
-        if (view.getId() == R.id.year_of_1921)
-            question8 = true;
-        else if ((view.getId() == R.id.year_of_1914) || (view.getId() == R.id.year_of_1934))
-            question8 = false;
-
-        // check question 10
-        if (view.getId() == R.id.city_tehran)
-            question10 = true;
-        else if ((view.getId() == R.id.city_baghdad) || (view.getId() == R.id.city_jerusalem))
-            question10 = false;
-    }
-
     private void checkEditTextAnswers() {
-
-
-
-        CheckBox attendedHarvard = (CheckBox) findViewById(R.id.attended_harvard);
-        boolean hasChocolate = attendedHarvard.isChecked();
-
-
-
-        // getResources method found on StackOverflow:
-        // https://stackoverflow.com/questions/7493287/android-how-do-i-get-string-from-resources-using-its-name
-
-
-
+        //  getResources method found on StackOverflow:
+        //  https://stackoverflow.com/questions/7493287/android-how-do-i-get-string-from-resources-using-its-name
         //
-        // https://stackoverflow.com/questions/6819604/comparison-of-two-strings-doesnt-work-in-android
+        //  equalsIgnoreCase method found on StackOverflow:
+        //  https://stackoverflow.com/questions/6819604/comparison-of-two-strings-doesnt-work-in-android
 
-        // check question3
-        question3 = answerThree.getText().toString().equalsIgnoreCase(getResources().getString(R.string.the_progressive_party));
-
-        // check question4
-        question4 = answerFour.getText().toString().equalsIgnoreCase(getResources().getString(R.string.sagamore_hill));
-
-        // check question7
-        question7 = answerSeven.getText().toString().equalsIgnoreCase(getResources().getString(R.string.warm_springs));
-
-        // check question8
-        question8 = answerEight.getText().toString().equalsIgnoreCase(getResources().getString(R.string.supreme_court));
-
-        // check question9
-        question9 = answerNine.getText().toString().equalsIgnoreCase(getResources().getString(R.string.social_security));
+        //  check all EditText views for correct answers
+        questionThreeCorrect = answerThree.getText().toString().equalsIgnoreCase(getResources().getString(R.string.the_progressive_party));
+        questionFourCorrect = answerFour.getText().toString().equalsIgnoreCase(getResources().getString(R.string.sagamore_hill));
+        questionSevenCorrect = answerSeven.getText().toString().equalsIgnoreCase(getResources().getString(R.string.warm_springs));
+        questionEightCorrect = answerEight.getText().toString().equalsIgnoreCase(getResources().getString(R.string.supreme_court));
+        questionNineCorrect = answerNine.getText().toString().equalsIgnoreCase(getResources().getString(R.string.social_security));
     }
 
     /**
-     * Method to calculate the number of correct answers.  Called by "displayScore" when the
-     * "Total Score" button is pressed at the end of the quiz.   Tests if the boolean for each
-     * question - "questionX" - is true, and if true adds 1 to "totalScore".
+     * Method to check if the CheckBox views in Question 6 are checked; in this instance, all check boxes must be checked
+     * in order for the answer to be correct; sets questionSixCorrect accordingly
+     */
+    private void checkCheckBoxAnswers() {
+        //  check all CheckBox views to see if they're selected
+        questionSixCorrect = (attendedHarvard.isChecked()) && (asstSecyNavy.isChecked()) && (governorNewYork.isChecked());
+    }
+
+    /**
+     * Method to calculate the number of correct answers.  Called by "displayScore" when the "Total Score" button
+     * is pressed at the end of the quiz; tests if the boolean for each question - "questionXXXCorrect" - is true,
+     * and if true adds 1 to "totalScore".
+     *
+     * Global boolean values were used (instead of calling the various view methods here) to keep the code clean and
+     * comprehensible;
      *
      * @return int total score -- the number of correct answers
      */
     private int calculateScore() {
         int totalScore = 0;
 
-        if (question1)
+        if (questionOneCorrect)
             totalScore += 1;
-        if (question2)
+        if (questionTwoCorrect)
             totalScore += 1;
-        if (question3)
+        if (questionThreeCorrect)
             totalScore += 1;
-        if (question4)
+        if (questionFourCorrect)
             totalScore += 1;
-        if (question5)
+        if (questionFiveCorrect)
             totalScore += 1;
-        if (question6)
+        if (questionSixCorrect)
             totalScore += 1;
-        if (question7)
+        if (questionSevenCorrect)
             totalScore += 1;
-        if (question8)
+        if (questionEightCorrect)
             totalScore += 1;
-        if (question9)
+        if (questionNineCorrect)
             totalScore += 1;
-        if (question10)
+        if (questionTenCorrect)
             totalScore += 1;
         return totalScore;
     }
@@ -191,15 +152,14 @@ public class MainActivity extends AppCompatActivity {
      * @param view of Button "Total Score"
      */
     public void displayScore(View view) {
-        int finalScore;
 
+        //  Call methods to check for the correct answers to the three types of questions
+        checkRadioButtonAnswers();
         checkEditTextAnswers();
-//        getPlayerName();
-        //  Call method to calculate number of correct answers
-        finalScore = calculateScore();
+        checkCheckBoxAnswers();
 
         // Toast to display final score
-        Toast.makeText(MainActivity.this, playerName + ", your score is:  " + finalScore,
+        Toast.makeText(MainActivity.this,  "Your score is:  " + calculateScore(),
                 Toast.LENGTH_LONG).show();
     }
 }
